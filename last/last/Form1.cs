ee5b9e0514aa713e8d56d7f66aa1971ea5cf7668
VladMakhun <147133088+VladMakhun@users.noreply.github.com>
2025-05-19 12:03:25 +0300

    Add files via upload

---
+++ last/last/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace last
+{
+    public partial class Form1 : Form
+    {
+
+        List<MovingObject> objects = new List<MovingObject>();
+        Random rand = new Random();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Paint += pictureBox1_Paint;
+            timer1.Tick += timer1_Tick;
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++) // Створити 10 об’єктів
+            {
+                float x = rand.Next(0, pictureBox1.Width - 30);
+                float y = rand.Next(0, pictureBox1.Height - 30);
+                float dx = (float)(rand.NextDouble() * 4 - 2);
+                float dy = (float)(rand.NextDouble() * 4 - 2);
+                int size = rand.Next(20, 40);
+                Color color = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+
+                objects.Add(new MovingObject(x, y, dx, dy, size, color));
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (var obj in objects)
+            {
+                obj.Move(pictureBox1.Width, pictureBox1.Height);
+            }
+
+            pictureBox1.Invalidate(); // Запускає перерисовку
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var obj in objects)
+            {
+                obj.Draw(e.Graphics);
+            }
+        }
+
+        private void pictureBox1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ last/last/MovingObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace last
+{
+    internal class MovingObject
+    {
+        public float X, Y;
+        public float DX, DY;
+        public int Size;
+        public Color Color;
+
+        public MovingObject(float x, float y, float dx, float dy, int size, Color color)
+        {
+            X = x;
+            Y = y;
+            DX = dx;
+            DY = dy;
+            Size = size;
+            Color = color;
+        }
+
+        public void Move(int width, int height)
+        {
+            X += DX;
+            Y += DY;
+
+            // Відбивання від країв
+            if (X < 0 || X + Size > width)
+                DX = -DX;
+            if (Y < 0 || Y + Size > height)
+                DY = -DY;
+        }
+
+        public void Draw(Graphics g)
+        {
+            using (Brush brush = new SolidBrush(Color))
+            {
+                g.FillEllipse(brush, X, Y, Size, Size);
+            }
+        }
+    }
+}
